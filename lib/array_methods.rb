0ea3d385a2feff8650450d3210d23cb907674687
Jack Overby <jroverby92@gmail.com>
2020-01-10 08:53:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   i = 0
-  while (array[i] != value_to_find) && (array[i]) {
+  while (array[i] != value_to_find) && (i < array.length) {
     i += 1
   }
   (i < array.length)? : i : nil
